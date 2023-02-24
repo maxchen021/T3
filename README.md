dfb98a771238b0648550d22a8441ba52a3e616f2
Max Chen <22202313+maxchen021@users.noreply.github.com>
2023-02-23 20:49:49 -0500

    add the ability to get text template data via url

--- README.md
+++ README.md
@@ -4,11 +4,27 @@ Simple text templating tool that turn any text template (config template, runboo
 No more manual find and replace. No more worries about if someone forgot to replace certain values.  
     
 You can access the tool here: https://maxchen021.github.io/T3/  
+If you want the ability to download text template data from url, then you will need to run your own instance via docker from the "Run your own local copy via docker" section.  
   
 ![screenshot 1](./images/screenshot1.png)
+  
+![screenshot 2](./images/screenshot2.png)
 
 ## Run your own local copy via docker
-Use the following command to run the container:
+If you are using v1.0.1 or later, use the following command to run the container:
 ```
-docker run -d -p 80:80 maxchen021/t3
+docker run -d -p 80:8080 maxchen021/t3
+```
+  
+If you are using v1.0.0, use the following command to run the container:
+```
+docker run -d -p 80:80 maxchen021/t3:v1.0.0
+```
+
+## Nginx ingress configuration
+If you are running it under kubernetes with nginx ingress, add the following config so it can handle long url:
+```
+nginx.ingress.kubernetes.io/server-snippet: |
+  http2_max_header_size 256k;
+  http2_max_field_size 256k;
 ```
---
+++ flask_app/app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
+import os, json
+import gunicorn.app.base
+import multiprocessing
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index_html():
+    return render_template("index.html")
+
+@app.route("/api/get-data-from-url")
+def get_data_from_url():
+    request_data = request.args
+    #print(request_data)
+    if request_data.get("url"):
+        response = requests.get(request_data["url"])
+        return_response = make_response(response.text)
+        return_response.status_code = response.status_code
+        return_response.headers.set("Content-Type", "text/plain; charset=utf-8")
+        return return_response
+    else:
+        return ""
+
+def number_of_gunicorn_workers():
+    """This calculate the number of gunicorn worker based on the number of cpu
+    Returns:
+        int: number of gunicorn workers
+    """
+    cpu_count = multiprocessing.cpu_count()
+    number_of_workers = 4
+    if cpu_count:
+        number_of_workers = cpu_count
+    print(f"number of workers: {number_of_workers}")
+    return number_of_workers
+
+
+class GunicornApplication(gunicorn.app.base.BaseApplication):
+    """This class is used to start the flask app using gunicorn
+    Args:
+        gunicorn (class): gunicorn base application class
+    """
+
+    def __init__(self, application, options=None):
+        """init function
+        Args:
+            application (class): Flask app class instance
+            options (dict, optional): dictionary containing the gunicorn config options. Defaults to None.
+        """
+        self.options = options or {}
+        self.application = application
+        super().__init__()
+
+    def load_config(self):
+        """Load the gunicorn config options"""
+        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        """Load the flask application
+        Returns:
+            class: Flask app class instance
+        """
+        return self.application
+
+
+if __name__ == "__main__":
+    # GUNICORN config settings / options
+    # https://docs.gunicorn.org/en/stable/settings.html#settings
+    options = {
+        "bind": "0.0.0.0:" + os.getenv("FLASK_HTTP_PORT", "8080"),
+        "workers": number_of_gunicorn_workers(),
+        "threads": 1,
+        "timeout": 300,
+        "keepalive": 300,
+        "limit_request_line": 0,
+        "capture_output": True,
+        "enable_stdio_inheritance": True,
+        "accesslog": "-",
+        "errorlog": "-",
+    }
+    GunicornApplication(app, options).run()
